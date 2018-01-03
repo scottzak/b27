86836a4a982db13f5bc09e8be4c3ff4b06f71ec9
Scott Zak <scottzak@gmail.com>
2018-01-03 21:35:18 +0000

    App component provides basic two-column layout.  With bootstrap and dummy test script.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My name is Scott Zak';
+  tagline = "...and I'm a developer!"
 }
