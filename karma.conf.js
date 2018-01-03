7dd4fb2ba52be7b7a8fa4ed2f7af572ef146cc97
Scott Zak <scottzak@gmail.com>
2018-01-03 20:05:45 +0000

    Set up karma for Cloud 9 environment.

--- karma.conf.js
+++ karma.conf.js
@@ -23,11 +23,14 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
+    port: 8080,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
+    // The hostname may be dynamically generated.  If you cannect connect through Preview, check that the hostnames match.
+    hostname: 'b2c17b8c17b84d089f0b251713739b97.vfs.cloud9.us-east-1.amazonaws.com',
+    runnerPort: 0,
     singleRun: false
   });
 };
