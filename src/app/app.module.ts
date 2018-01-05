cd2db212628f62bf3066ca3e454e8a05f379c8c5
Scott Zak <scottzak@gmail.com>
2018-01-05 19:51:12 +0000

    Create content-container module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ContentContainerComponent } from './content-container/content-container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentContainerComponent
   ],
   imports: [
     BrowserModule,
